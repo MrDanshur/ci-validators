225d94402bd4b7b5dfd18d33002979d474c6644d
Alexander Krasov <akrasov@itsupportme.by>
2018-08-13 12:15:06 +0300

    Rregister Compiler Pass in Bundle

--- ISCIValidatorsBundle.php
+++ ISCIValidatorsBundle.php
@@ -2,8 +2,16 @@
 
 namespace IS\CIValidatorsBundle;
 
+use IS\CIValidatorsBundle\DependencyInjection\Compiler\CIValidatorsCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ISCIValidatorsBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new CIValidatorsCompilerPass());
+    }
 }
